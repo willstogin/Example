f24f2843a6d374550c4421abc7b627fd4a33b69f
William <williamstogin2018@u.northwestern.edu>
2015-11-17 21:26:30 -0600

    Added basic input and output

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 
 public class Main {
 
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Gets input from user
+		Scanner consoleIn = new Scanner(System.in);
+		String nextLine;
 
+		System.out.println("Hi, I'm an alarm clock! You can: ask me for time and stuff!");
+		System.out.println("Ask me what time it is!");
+		
+		nextLine = consoleIn.nextLine();
+		
+		System.out.println("You asked for: " + nextLine);
 	}
 
 }
