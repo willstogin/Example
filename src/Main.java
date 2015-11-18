c46fcecc0d82d859c38203c63609668887b8329d
William <williamstogin2018@u.northwestern.edu>
2015-11-17 21:53:29 -0600

    Added stubs for the commands

--- src/Main.java
+++ src/Main.java
@@ -15,32 +15,76 @@ public class Main {
 		
 		// Gets input from user
 		Scanner consoleIn = new Scanner(System.in);
-		String nextLine;
+		String nextLine = "";
+		boolean keepGoing = true;
 
 		System.out.println("Hi, I'm an alarm clock! You can: ask me for time and stuff!");
-		System.out.println("Ask me what time it is!");
+		 
 		
-		nextLine = consoleIn.nextLine();
 		
-		System.out.println("You asked for: " + nextLine);
+		while (keepGoing) {
+			
+			System.out.println("What would you like to do?");
+			nextLine = consoleIn.nextLine();
+			
+			switch (nextLine) {
+			case GET_TIME:
+				//TODO
+				System.out.println("You asked for: " + nextLine);
+				break;
+			case SET_TIME:
+				// TODO
+				System.out.println("You asked for: " + nextLine);
+				break;
+			case SET_ALARM:
+				//TODO 
+				System.out.println("You asked for: " + nextLine);
+				break;
+			case GET_ALARM:
+				// TODO
+				System.out.println("You asked for: " + nextLine);
+				break;
+			case EXIT:
+				// make keepGoing false to break out of the while
+				keepGoing = false;
+				System.out.println("You asked for: " + nextLine);
+				break;
+			case STEP:
+				// TODO
+				System.out.println("You asked for: " + nextLine);
+				break;
+			case HELP:
+				//TODO print out help instructions
+
+				System.out.println("You asked for: " + nextLine);
+				break;
+			default:
+				System.out.println("I don't know how to do that.\nEnter " + HELP + "for a list of commands");
+				
+			}
+			
+		}
+		
+		System.out.println("Goodbye!");
 	}
 	
 	
 	
 	
 	/* *************************** COMMANDS ******************************* */
-	private final String GET_TIME = "time?";
+	private static final String GET_TIME = "time?";
 	
-	private final String SET_TIME = "set time";
+	private static final String SET_TIME = "set time";
 	
-	private final String GET_ALARM = "alarm?";
+	private static final String GET_ALARM = "alarm?";
 	
-	private final String SET_ALARM = "set alarm";
+	private static final String SET_ALARM = "set alarm";
 	
-	private final String EXIT = "exit";
+	private static final String EXIT = "exit";
 	
-	private final String STEP = "wait";
+	private static final String STEP = "wait";
 	
+	private static final String HELP = "help";
 	
 	
 
