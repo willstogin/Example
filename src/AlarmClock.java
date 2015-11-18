b6393f2f12e24643eb1375b54866c5cd042dd9be
William <williamstogin2018@u.northwestern.edu>
2015-11-17 22:18:11 -0600

    Added getters and setters for time ad alarm

--- src/AlarmClock.java
+++ src/AlarmClock.java
@@ -27,13 +27,47 @@ public class AlarmClock {
 	
 	
 	
-	
+	/** 
+	 * Tells whether or not the alarm is ringing.
+	 * @return boolean: whether the alarm is ringing.
+	 */
 	public boolean isRinging( ) { return ringing; }
 	
+	/**
+	 * Gets the current time in string form
+	 * @return String: "hour:minute"
+	 */
+	public String getTime( ) {
+		return String.format("%d:%d", current.getHour(), current.getMinute());
+	}
 	
+	/**
+	 * Gets the alarm time in string form
+	 * @return String: "hour:minute"
+	 */
+	public String getAlarm() {
+		return String.format("%d:%d", alarm.getHour(), alarm.getMinute());
+	}
 	
+	/**
+	 * Sets the current time to be the hour and minute specified.
+	 * @param hour
+	 * @param minute
+	 */
+	public void setTime(int hour, int minute) {
+		current.setHour(hour);
+		current.setMinute(minute);
+	}
 	
-	
+	/**
+	 * Sets the alarm time to be the hour and minute specified
+	 * @param hour int
+	 * @param minute int
+	 */
+	public void setAlarm(int hour, int minute) {
+		alarm.setHour(hour);
+		alarm.setMinute(minute);
+	}
 	
 	
 	
