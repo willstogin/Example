baebbe1474178bae1fca3e6c8b64c5533cb7d1f5
William <williamstogin2018@u.northwestern.edu>
2015-11-18 14:46:50 -0600

    printed Hello world

---
+++ src/NancyHello.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author William
+ *
+ */
+public class NancyHello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+
+}
